e75f7742bd8de99aeec7e8d99e9698851d58965d
Alessandro Fresneda <alessandro.fresneda@codeandtheory.com>
2024-02-25 23:52:10 -0300

    Add DM message handling and assistant integration

--- src/events/message-create-handler.ts
+++ src/events/message-create-handler.ts
@@ -1,7 +1,39 @@
 import { ChannelType, Message } from "discord.js";
+import {
+  createThreadMessage,
+  getMessageText,
+  getOrCreateAssistant,
+  getOrCreateThread,
+  listThreadMessages,
+  removeMessageAnnotations,
+  runThreadMessage,
+  waitForThreadRun,
+} from "../assistant";
+import { logger } from "../lib/logger";
 
 export async function messageCreateHandler(message: Message<boolean>) {
   if (message.channel.type === ChannelType.DM && !message.author.bot) {
-    await message.reply("You sent a DM: " + message.content);
+    logger.info(
+      `Received message from ${message.author.id}: ${message.content}`
+    );
+
+    const assistantId = await getOrCreateAssistant();
+    const threadId = await getOrCreateThread(message.author.id);
+    await createThreadMessage(threadId, message.content);
+    const threadRun = await runThreadMessage(threadId, assistantId);
+    await waitForThreadRun(threadId, threadRun.id);
+    const threadMessages = await listThreadMessages(threadId);
+    const messageContent = await getMessageText(threadMessages);
+
+    if (!messageContent) {
+      logger.error(`Failed to get message content for thread ${threadId}`);
+      await message.reply("I'm sorry, I didn't understand that.");
+      return;
+    }
+
+    const cleanMessage = removeMessageAnnotations(messageContent);
+
+    logger.info(`Sending message to ${message.author.id}: ${cleanMessage}`);
+    await message.reply(cleanMessage);
   }
 }
