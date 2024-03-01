51f7250d87441146e86a3c765ba5f500bdd6b284
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-02-29 23:27:58 -0300

    Refactor commands.ts: Remove askCommand

--- src/commands.ts
+++ src/commands.ts
@@ -1,15 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export const askCommand = new SlashCommandBuilder()
-  .setName("ask")
-  .setDescription("Ask a question")
-  .addStringOption((option) =>
-    option
-      .setName("question")
-      .setDescription("The question to ask")
-      .setRequired(true),
-  );
-
 export const emailCommand = new SlashCommandBuilder()
   .setName("email")
   .setDescription("Set your email")
