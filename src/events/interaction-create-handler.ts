6f3b5bff6d7a00ba110f3b2a49be96528d7d2ea4
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-05 22:46:34 -0300

    Refactor email command handler to use interaction.editReply() instead of interaction.reply().

--- src/events/interaction-create-handler.ts
+++ src/events/interaction-create-handler.ts
@@ -18,18 +18,19 @@ export async function interactionCreateHandler(
 const EmailCommandParser = z.coerce.string().email();
 
 async function emailCommandHandler(interaction: CommandInteraction<CacheType>) {
+  interaction.deferReply();
   const email = interaction.options.get("email")?.value;
   const parsedEmail = EmailCommandParser.safeParse(email);
 
   if (!parsedEmail.success) {
-    await interaction.reply(
+    await interaction.editReply(
       "O e-mail informado é inválido. Por favor, tente novamente.",
     );
     return;
   }
 
   await redis.set(`discord:${interaction.user.id}:email`, parsedEmail.data);
-  await interaction.reply(`
+  await interaction.editReply(`
     Muito bom! Seu e-mail foi salvo com sucesso. Agora podemos continuar com as suas dúvidas.
   `);
 }
