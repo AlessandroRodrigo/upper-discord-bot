fd793b9f5e906d9333819177e6fec208bba5e3fe
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-06 13:52:57 -0300

    Update interaction-create-handler.ts to include user's username in log messages

--- src/events/interaction-create-handler.ts
+++ src/events/interaction-create-handler.ts
@@ -7,16 +7,20 @@ export async function interactionCreateHandler(
   interaction: Interaction<CacheType>,
 ) {
   logger.info(
-    `Received interaction of type ${interaction.type} from user ${interaction.user.id}`,
+    `Received interaction of type ${interaction.type} from user ${interaction.user.id} - ${interaction.user.username}`,
   );
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
 
   if (commandName === "email") {
-    logger.info(`Handling email command for user ${interaction.user.id}`);
+    logger.info(
+      `Handling email command for user ${interaction.user.id} - ${interaction.user.username}`,
+    );
     await emailCommandHandler(interaction as CommandInteraction<CacheType>);
-    logger.info(`Handled email command for user ${interaction.user.id}`);
+    logger.info(
+      `Handled email command for user ${interaction.user.id} - ${interaction.user.username}`,
+    );
     return;
   }
 }
