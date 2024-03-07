f23ee55fd60bda23768ed63be17a7f08800b1748
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-07 10:12:32 -0300

    Commented out email verification and subscription verification code

--- src/events/message-create-handler.ts
+++ src/events/message-create-handler.ts
@@ -1,8 +1,8 @@
 import { BusinessLayer } from "@/business/business-layer";
 import { AssistantFactory } from "@/factories/assistant";
-import { checkIfSubscriptionIsActive } from "@/lib/hotmart";
+// import { checkIfSubscriptionIsActive } from "@/lib/hotmart";
 import { logger } from "@/lib/logger";
-import { redis } from "@/lib/redis";
+// import { redis } from "@/lib/redis";
 import { Message } from "discord.js";
 
 export async function messageCreateHandler(message: Message<boolean>) {
@@ -21,17 +21,20 @@ export async function messageCreateHandler(message: Message<boolean>) {
       return;
     }
 
-    await handleEmailVerification(message, async (email, message) => {
-      logger.info(`Processing message with email verification.`);
-      handleSubscriptionVerification(
-        message,
-        email,
-        async (isActive, message) => {
-          logger.info(`Processing message with subscription verification.`);
-          processMessageWithAssistant(message);
-        },
-      );
-    });
+    logger.info(`Processing message with subscription verification.`);
+    processMessageWithAssistant(message);
+
+    // await handleEmailVerification(message, async (email, message) => {
+    //   logger.info(`Processing message with email verification.`);
+    //   handleSubscriptionVerification(
+    //     message,
+    //     email,
+    //     async (isActive, message) => {
+    //       logger.info(`Processing message with subscription verification.`);
+    //       processMessageWithAssistant(message);
+    //     },
+    //   );
+    // });
 
     return;
   }
@@ -55,42 +58,42 @@ async function processMessageWithAssistant(message: Message<boolean>) {
   await message.reply(assistantMessage);
 }
 
-async function handleEmailVerification(
-  message: Message,
-  callback: (email: string, message: Message) => void,
-) {
-  const foundEmail = await redis.get(`discord:${message.author.id}:email`);
+// async function handleEmailVerification(
+//   message: Message,
+//   callback: (email: string, message: Message) => void,
+// ) {
+//   const foundEmail = await redis.get(`discord:${message.author.id}:email`);
 
-  if (!foundEmail) {
-    await message.channel.send(
-      "Ei, estou aqui para te ajudar, mas notei que seu e-mail ainda não está configurado.",
-    );
-    await message.channel.send(
-      "Por favor use o comando `/email 'seu e-mail'` para configurar seu e-mail. Lembre-se de utilizar o mesmo da Hotmart.",
-    );
-    return;
-  }
+//   if (!foundEmail) {
+//     await message.channel.send(
+//       "Ei, estou aqui para te ajudar, mas notei que seu e-mail ainda não está configurado.",
+//     );
+//     await message.channel.send(
+//       "Por favor use o comando `/email 'seu e-mail'` para configurar seu e-mail. Lembre-se de utilizar o mesmo da Hotmart.",
+//     );
+//     return;
+//   }
 
-  callback(foundEmail, message);
-}
+//   callback(foundEmail, message);
+// }
 
-async function handleSubscriptionVerification(
-  message: Message,
-  email: string,
-  callback: (isActive: boolean, message: Message) => void,
-) {
-  const isSubscriptionActive = await checkIfSubscriptionIsActive(email);
+// async function handleSubscriptionVerification(
+//   message: Message,
+//   email: string,
+//   callback: (isActive: boolean, message: Message) => void,
+// ) {
+//   const isSubscriptionActive = await checkIfSubscriptionIsActive(email);
 
-  if (!isSubscriptionActive) {
-    logger.info(`Subscription is not active for ${email}`);
-    await message.reply(
-      "Parece que seu e-mail não está ativo na Hotmart, você tem certeza que digitou corretamente?",
-    );
-    await message.reply(
-      "Por favor, use o comando `/email 'seu e-mail'` para configurar seu e-mail novamente.",
-    );
-    return;
-  }
+//   if (!isSubscriptionActive) {
+//     logger.info(`Subscription is not active for ${email}`);
+//     await message.reply(
+//       "Parece que seu e-mail não está ativo na Hotmart, você tem certeza que digitou corretamente?",
+//     );
+//     await message.reply(
+//       "Por favor, use o comando `/email 'seu e-mail'` para configurar seu e-mail novamente.",
+//     );
+//     return;
+//   }
 
-  callback(isSubscriptionActive, message);
-}
+//   callback(isSubscriptionActive, message);
+// }
